84f265b841c9d11d22b88642d44c744cc14e5ad9
Caio Bandeira <https://github.com/Kzarr/Ingressos.git>
2025-05-22 04:22:22 +0000

    forth commit

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const mangaRoutes = require('./routes/mangaRoutes');
 const app = express();
 const PORT = 8000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));4
+app.use('/chapters.json', (req, res) => {
+  res.sendFile(path.join(__dirname, 'data', 'chapters.json'));
+});
 app.use('/', mangaRoutes);
 
 app.get('/', (req, res) => {
